12d921f2714d8b57f6506f504d4db8b938930d49
Artem Pronkin <artempro1994@gmail.com>
2023-11-23 13:01:12 +0500

    query

--- src/main/java/com/example/security_example/repositories/UserRepository.java
+++ src/main/java/com/example/security_example/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.security_example.repositories;
 
 import com.example.security_example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.username = :username")
     User findUserByUsername(String username);
 }
